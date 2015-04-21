5db9c9c91c660fff4e210f77d147d4f9c33d0c7d
Peter Den Hartog <pddenhar@gmail.com>
2015-04-21 11:22:20 -0500

    Added memory subfolder for memory tools and eventuall the dobj related to it

---
+++ src/memory/memory.c
@@ -0,0 +1,5 @@
+//
+// Created by peter on 4/21/15.
+//
+
+#include "memory.h"
---
+++ src/memory/memory.h
@@ -0,0 +1,10 @@
+//
+// Created by Peter Den Hartog (pddenhar @ github) on 4/21/15.
+//
+
+#ifndef EDSM_MEMORY_H
+#define EDSM_MEMORY_H
+
+
+
+#endif //PROJECT_MEMORY_H
