716c4929af91b513ee6018f087b486d45b6e07ac
Alex Sherman <asherman1024@gmail.com>
2015-04-23 20:43:14 -0500

    Client side changes for nbody.py

--- test/nbody.py
+++ test/nbody.py
@@ -11,10 +11,15 @@ screen_size = 400
 screen = pygame.display.set_mode((screen_size, screen_size))
 #points = [[0,0,1], [0,1,1], [1,0,1], [1,1,1]]
 points = []
-point_count = 35
+point_count = 2
 for x in range(point_count):
     for y in range(point_count):
         points.append([1.0 * x / point_count, 1.0 * y / point_count,1])
+
+
+b1, b2 = server.init_simulation(points)
+
+print b1, b2
 
 while True:
     screen.fill((0,0,0))
@@ -22,7 +27,7 @@ while True:
         pygame.draw.circle(screen, (128,128,128), (int(screen_size * point[0]), int(screen_size *point[1])), int(point[2] * 3), 0)
 
     pygame.display.update()
-    points = server.run_simulation(points, 0.001, 1, 1)
+    points = server.run_simulation(len(points), b1, b2, 0.01, 1, 1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
